be958ceaab3aba0dd44a9edd963260c4b212c351
John Hossler <john.m.hossler@gmail.com>
2018-03-07 15:21:35 -0600

    Added some other api calls, wrote some tests

--- src/twitter_mood/mood_gatherer.py
+++ src/twitter_mood/mood_gatherer.py
@@ -1,9 +1,15 @@
-from ._tools import MoodInterface
+from .mood_interface import MoodInterface
+
 
 class TwitterMoodGatherer:
 
-    def __init__(self, twitter_interface):
+    def __init__(self, twitter_interface, sentiment_interface):
         self.__twitter_interface = twitter_interface
+        self.__sentiment_interface = sentiment_interface
 
     def get_mood(self, query):
-        return MoodInterface()
+        tweets = self.__twitter_interface.query(query)
+        if len(tweets) == 0:
+            return MoodInterface()
+        return sum([self.__sentiment_interface.analyze(tweet)
+                    for tweet in tweets]) / len(tweets)
--- tests/test_get_mood.py
+++ tests/test_get_mood.py
@@ -1,9 +1,38 @@
 from unittest.mock import MagicMock
 from twitter_mood.mood_gatherer import TwitterMoodGatherer
+from twitter_mood.mood_interface import MoodInterface
 
 class TestGetMoodSuite:
     def test_get_mood_empty_tweets(self):
-        mood_gatherer = TwitterMoodGatherer(MagicMock())
+        twitter_mock = MagicMock()
+        sentiment_mock = MagicMock()
+        sentiment_mock.analyze.return_value = MoodInterface()
+        mood_gatherer = TwitterMoodGatherer(twitter_mock, sentiment_mock)
         mood = mood_gatherer.get_mood(query='foo')
         assert mood.polarity == 0
         assert mood.subjectivity == 0
+
+    def test_get_mood_single_tweet(self):
+        twitter_mock = MagicMock()
+        sentiment_mock = MagicMock()
+        twitter_mock.query.return_value = ['bar']
+        sentiment_mock.analyze.return_value = MoodInterface(-0.1, 0.8)
+
+        mood_gatherer = TwitterMoodGatherer(twitter_mock, sentiment_mock)
+
+        mood = mood_gatherer.get_mood(query='foo')
+        assert mood.polarity == -0.1
+        assert mood.subjectivity == 0.8
+
+    def test_get_mood_multiple_tweets(self):
+        twitter_mock = MagicMock()
+        sentiment_mock = MagicMock()
+        twitter_mock.query.return_value = ['bar', 'baz']
+        sentiment_mock.analyze.side_effect = [MoodInterface(-0.3, 1),
+                                              MoodInterface(-0.1, 0.8)]
+
+        mood_gatherer = TwitterMoodGatherer(twitter_mock, sentiment_mock)
+
+        mood = mood_gatherer.get_mood(query='foo')
+        assert mood.polarity == -0.2
+        assert mood.subjectivity == 0.9
