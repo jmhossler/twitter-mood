8002c5ba811196d304223acbda0d0b4d8ee8e0ed
John Hossler <john.m.hossler@gmail.com>
2018-03-19 20:27:06 -0500

    Did rework, put an example in

--- README.md
+++ README.md
@@ -3,13 +3,38 @@ twitter-mood
 [![Build Status](https://travis-ci.org/jmhossler/twitter-mood.svg?branch=master)](https://travis-ci.org/jmhossler/twitter-mood)
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
-Add a short description here!
+twitter-mood is a python tool meant to simplify sentiment analysis of twitter queries.
 
 
 Description
 -----------
 
-A longer description of your project goes here...
+Using textblob and python-twitter, twitter-mood aggregates sentiment analysis of
+tweets from twitter matching a specific query.
+
+Usage
+-----
+
+`from twitter_mood import TwitterMoodGatherer
+import twitter
+
+consumer_key='consumer_key'
+consumer_secret='consumer_secret'
+access_token_key='access_token'
+access_token_secret='access_token_secret'
+
+
+twitter_api = twitter.Api(consumer_secret=consumer_secret,
+                          api_token_key=api_token_key,
+                          api_token_secret=api_token_secret,
+                          consumer_key=consumer_key)
+
+twitter_mood_tool = TwitterMoodGatherer(twitter_api, query)
+
+twitter_mood_tool.gather_tweets()
+
+twitter_mood_tool.analyze_mood()  # return Sentiment Aggregate
+`
 
 
 Note
--- src/twitter_mood/mood_gatherer.py
+++ src/twitter_mood/mood_gatherer.py
@@ -1,15 +1,30 @@
-from .mood_interface import MoodInterface
+import collections
+
+from textblob import TextBlob
 
 
 class TwitterMoodGatherer:
 
-    def __init__(self, twitter_interface, sentiment_interface):
-        self.__twitter_interface = twitter_interface
-        self.__sentiment_interface = sentiment_interface
+    def __init__(self, twitter_api, query):
+        self.__twitter_api = twitter_api
+        self.__query = query
+        self.__sentiment_tool = TextBlob
+        self.__tweets = []
+        self.__sentiment_tuple = collections.namedtuple(
+                'Sentiment',
+                ['polarity', 'subjectivity'])
 
-    def get_mood(self, query):
-        tweets = self.__twitter_interface.query(query)
-        if len(tweets) == 0:
-            return MoodInterface()
-        return sum([self.__sentiment_interface.analyze(tweet)
-                    for tweet in tweets]) / len(tweets)
+    def get_mood(self):
+        if len(self.__tweets) == 0:
+            return self.__sentiment_tool('')
+        sentiments = [self.__sentiment_tool(tweet).sentiment
+                      for tweet in self.__tweets]
+        print(sentiments)
+        polarity_avg = sum(sentiment.polarity
+                           for sentiment in sentiments) / len(self.__tweets)
+        subjectivity_avg = sum(sentiment.subjectivity
+                               for sentiment in sentiments) / len(self.__tweets)
+        return self.__sentiment_tuple(polarity_avg, subjectivity_avg)
+
+    def gather_tweets(self):
+        self.__tweets = self.__twitter_api.GetSearch(term=self.__query)
