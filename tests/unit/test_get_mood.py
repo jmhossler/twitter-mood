b75964ed5c4dc576a967e1230e62492886c4837b
John Hossler <john.m.hossler@gmail.com>
2018-04-19 15:19:18 -0500

    Added way to get timeline of sentiments (#6)

--- tests/unit/test_get_mood.py
+++ tests/unit/test_get_mood.py
@@ -43,3 +43,30 @@ class TestGetMoodSuite:
         mood = mood_gatherer.get_mood()
         assert mood.polarity == -0.2
         assert mood.subjectivity == 0.9
+
+    @patch('twitter_mood.mood_gatherer.TextBlob')
+    def test_get_moods(self, text_mock):
+        twitter_mock = MagicMock()
+        twitter_mock.GetSearch.return_value = [
+                MagicMock(text='bar', created_at_in_seconds=1),
+                MagicMock(text='baz', created_at_in_seconds=2)]
+
+        sentiment_a = MagicMock(
+                sentiment=MagicMock(polarity=-0.3, subjectivity=1)),
+        sentiment_b = MagicMock(
+                sentiment=MagicMock(polarity=-0.1, subjectivity=0.8))
+        text_mock.side_effect = [
+                sentiment_a,
+                sentiment_b,
+                ]
+
+        mood_gatherer = TwitterMoodGatherer(twitter_mock, 'foo')
+
+        time_tuple = mood_gatherer._TwitterMoodGatherer__time_tuple
+
+        mood_gatherer.gather_tweets()
+        moods = mood_gatherer.get_moods()
+        assert moods == [time_tuple(sentiment=sentiment_a,
+                                    epoch_time=1),
+                         time_tuple(sentiment=sentiment_b,
+                                    epoch_time=2)]
